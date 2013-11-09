171071b50543fa28f738cf0319f64965a42a2bb8
Michaël Hoste <michael.hoste@gmail.com>
2013-11-09 19:36:56 +0100

    new folder for firefox-os version

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 rsync -ru _site/* deploy@xmoto.io:/home/deploy/apps/xmoto --delete
 ssh deploy@xmoto.io 'chmod -R +r /home/deploy/apps/xmoto'
+rsync -ru firefox-os/* deploy@xmoto.io:/home/deploy/apps/xmoto/firefox-os --delete
