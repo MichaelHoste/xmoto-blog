c69d07d931a72e00413816aac5a4bde65b580502
Michaël Hoste <michael.hoste@gmail.com>
2013-11-30 13:53:17 +0100

    gitignore

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rsync -ru _site/* deploy@xmoto.io:/home/deploy/apps/xmoto --delete
+rsync -ru data/* deploy@xmoto.io:/home/deploy/apps/xmoto/data --delete
 ssh deploy@xmoto.io 'chmod -R +r /home/deploy/apps/xmoto'
-rsync -ru firefox-os/* deploy@xmoto.io:/home/deploy/apps/xmoto/firefox-os --delete
